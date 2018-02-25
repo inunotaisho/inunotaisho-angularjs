4102ac1890cdf7a81a463281bc23747ab0f2baf7
inunotaisho26 <ebfairweather@gmail.com>
2018-02-25 17:36:07 -0500

    updating contact

--- src/views/contact/contact.vc.js
+++ src/views/contact/contact.vc.js
@@ -3,11 +3,7 @@ import angular from 'angular';
 angular.module('contact', [])
 .controller('contactCtrl', ($scope, $http) => {
 
-    function noName(){
-        $http.get('/admin')
-        .then(users => {
-            console.log('rrrrrrrr', users);
-            $scope.emails = users;
-        }).catch(err => console.log('shit got real!!', err));
-    }
+    let submit = function(){
+        
+    };
 });
