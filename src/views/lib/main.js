a25f9519140168f2974bf7f65700a50c4b6da07b
inunotaisho26 <ebfairweather@gmail.com>
2018-03-09 17:29:23 -0500

    disabling two routes unless logged in

--- src/views/lib/main.js
+++ src/views/lib/main.js
@@ -3,15 +3,17 @@ import bootstrap from 'bootstrap';
 import angular from 'angular';
 import ngResource from 'angular-resource';
 import ngRoute from 'angular-route';
+import froala from 'angular-froala';
 import blog from '../blog/blog.vc';
 import errors from '../error/error.vc';
 import navbar from '../navbar/navbar.dir.js';
+import editor from '../write/froala/froala.dir';
 import login from '../login/login.vc';
 import profile from '../profile/profile.vc.js';
 import write from '../write/write.vc';
 import contact from '../contact/contact.vc';
 import reg from '../reg/reg.vc';
-import '../authentication.js';
+import authModule from '../../services/authentication/authentication.js';
 import homeTemplate from '../home/home.vc.html';
 import educationTemplate from '../education/ed.vc.html';
 import errorTemplate from '../error/error.vc.html';
@@ -23,7 +25,11 @@ import writeTemplate from '../write/write.vc.html';
 import contactTemplate from '../contact/contact.vc.html';
 import regTemplate from '../reg/reg.vc.html';
 
- angular.module('myApp', ['ngRoute', 'ngResource','navbar','errors','blog','login','profile','write','contact','reg','authModule'])
+ angular.module('myApp', ['ngRoute', 'ngResource','navbar','errors', 'editor','blog','login','profile','write','contact','reg','authModule', 'froala'])
+    .value('froalaConfig', {
+        toolbarInline: false,
+        placeholderText: 'Enter Text Here'
+    })
     .config(($routeProvider, $locationProvider) => {
         $routeProvider
         .when('/', {
@@ -49,13 +55,23 @@ import regTemplate from '../reg/reg.vc.html';
             template: profileTemplate,
             controller:'profileCtrl',
             controllerUrl:'../profile/profile.vc.js',
-            isLogin: true
+            resolve : {
+                //This function is injected with the AuthService where you'll put your authentication logic
+                'auth' : function(AuthService){
+                    return AuthService.authenticate();
+                }
+            }
         })
         .when('/write', {
             template: writeTemplate,
             controller:'writeCtrl',
             controllerUrl:'../write/write.vc.js',
-            isLogin: true
+            resolve : {
+                //This function is injected with the AuthService where you'll put your authentication logic
+                'auth' : function(AuthService){
+                    return AuthService.authenticate();
+                }
+            }
         })
         .when('/contact', {
             template: contactTemplate,
@@ -75,4 +91,25 @@ import regTemplate from '../reg/reg.vc.html';
         .otherwise({
             redirectTo: '/'
         });
+    }).run(function($rootScope, $location){
+        //If the route change failed due to authentication error, redirect them out
+        $rootScope.$on('$routeChangeError', function(event, current, previous, rejection){
+            if(rejection === 'Not Authenticated'){
+                console.log('Zombie, I\'m here')
+                $location.path('/login');
+            }
+        })
+    }).factory('AuthService', function($q, authService){
+        return {
+            authenticate : function(){
+                //Authentication logic here
+                if(authService.getIsLoggedIn()){
+                    //If authenticated, return anything you want, probably a user object
+                    return true;
+                } else {
+                    //Else send a rejection
+                    return $q.reject('Not Authenticated');
+                }
+            }
+        }
     });
