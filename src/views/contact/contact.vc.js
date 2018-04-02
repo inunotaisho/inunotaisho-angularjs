8b6d8f84979aa2ad89cf2e31679eeca15e368bf0
inunotaisho26 <ebfairweather@gmail.com>
2018-04-02 18:23:00 -0400

    contact controller and html completed

--- src/views/contact/contact.vc.js
+++ src/views/contact/contact.vc.js
@@ -3,7 +3,31 @@ import angular from 'angular';
 angular.module('contact', [])
 .controller('contactCtrl', ($scope, $http) => {
 
-    let submit = function(){
-        
-    };
+    $scope.formData = function(isValid, error) {
+        this.formInput = {
+            firstName:$("input[name='firstName']").val(),
+            lastName:$("input[name='lastName']").val(),
+            email:$("input[name='email']").val(),
+            subject:$("input[name='subject']").val(),
+            message: $("textarea[name='message']").val()
+        }
+
+        if(isValid){
+            console.log(this.formInput);
+        } else {
+            console.log(error);
+        }
+
+        $http.post('/contact', this.formInput)
+        .then(data => {
+            if(data.status === 200) {
+                console.log('response: ', data);
+            }
+        }).catch(error => {
+            console.log('error: ', error);
+        });
+    }
+
+    $scope.showAlert = true;
+
 });
